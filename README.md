4ecf4051ed991bc3b8489620030c2d2c91dbbaaf
laobocheng <1063779828@qq.com>
2019-04-29 11:56:23 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AutoHideSoftInputLayout
 自动隐藏键盘布局
+
+基于帧布局实现的自动隐藏键盘布局，使用方法和帧布局一样
+
+- **OnHideListener** 接口监听隐藏事件
+- **hideSoftInput** 函数可直接调用隐藏键盘
---
+++ src/main/java/cc/xiaobaicz/autohidesoftinputlayout/AutoHideSoftInputLayout.java
@@ -0,0 +1,80 @@
+package cc.xiaobaicz.autohidesoftinputlayout;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.FrameLayout;
+
+/**
+ * 自动隐藏键盘布局
+ * @author BC
+ */
+public class AutoHideSoftInputLayout extends FrameLayout {
+
+    /**
+     * 隐藏监听接口
+     */
+    public interface OnHideListener {
+        /**
+         * 隐藏回调函数
+         */
+        void onHide();
+    }
+
+    private OnHideListener mOnHideListener;
+
+    public AutoHideSoftInputLayout(Context context) {
+        super(context);
+    }
+
+    public AutoHideSoftInputLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoHideSoftInputLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AutoHideSoftInputLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    /**
+     * 设置隐藏监听
+     */
+    public void setOnHideListener(OnHideListener listener) {
+        mOnHideListener = listener;
+    }
+
+    /**
+     * 隐藏键盘
+     */
+    public void hideSoftInput() {
+        final InputMethodManager imf = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imf != null && imf.isAcceptingText()) {
+            imf.hideSoftInputFromWindow(getWindowToken(), 0);
+            clearFocus();
+            onHide();
+        }
+    }
+
+    private void onHide() {
+        if (mOnHideListener != null)
+            mOnHideListener.onHide();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        boolean active = super.dispatchTouchEvent(ev);
+        //无处理时，可隐藏键盘
+        if (!active) {
+            hideSoftInput();
+        }
+        return active;
+    }
+
+}
